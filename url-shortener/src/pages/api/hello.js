5492947db3d234c7078d4ff1a8e84aab1eec0405
Milo CARTAL <105420069+milocartal@users.noreply.github.com>
2023-03-06 17:31:44 +0100

    marche pas lol

--- url-shortener/src/pages/api/hello.js
+++ url-shortener/src/pages/api/hello.js
@@ -1,5 +1,38 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import Redis from 'redis'
+import configuration from '~/configuration';
+
+let redis = new Redis(process.env.REDIS_URL)
+
+function getRedisConfiguration() {
+  return configuration.redis;
+}
+
+export default async (req, res) => {
+   let start = Date.now();
+   let cache = await redis.get("cache")
+   cache = JSON.parse(cache)
+   let result = {}
+   if (cache) {
+       console.log("loading from cache")
+       result.data = cache
+       result.type = "redis"
+       result.latency = Date.now() - start;
+       return res.status(200).json(result)
+   } else {
+       console.log("loading from api")
+       start = Date.now();
+       return fetch('https://coronavirus-19-api.herokuapp.com/countries')
+           .then(r => r.json())
+           .then(data => {
+               data.sort(function (a, b) {
+                   return b.todayCases - a.todayCases;
+               });
+               result.data = data.splice(1, 11)
+               result.type = "api"
+               result.latency = Date.now() - start;
+               redis.set("cache", JSON.stringify(result.data), "EX", 60)
+               return res.status(200).json(result)
+           })
+   }
 }
